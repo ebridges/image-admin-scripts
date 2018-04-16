33b5890ce5f3a10dbd3fdb94bcc95ddc047fc769
Edward Bridges <github@eqbridges.com>
2018-04-16 09:23:47 -0400

    use lib function to find create date, instead of shelling to exiftool

--- print-missing-create-date.pl
+++ print-missing-create-date.pl
@@ -2,14 +2,27 @@
 
 use strict;
 
+require './util.pl';
+
 for(<DATA>){
     chomp;
-    my @result = `exiftool -q -if '(not \$CreateDate or (\$CreateDate eq "0000:00:00 00:00:00"))' -csv -common "$_"`;
-    if(scalar(@result) > 1) {
-	chomp @result;
-	my @fields = split /,/, $result[1];
-	print "$fields[0]\n";
+    die "unable to locate file [$_]: $!\n"
+        unless -e;
+
+    my $date = &tag($_, ('CreateDate', 'DateTimeOriginal'));
+    next
+	unless $date;
+
+    if(not $date or $date eq '0000:00:00 00:00:00') {
+	print "$_\n";
     }
+    
+    # my @result = `exiftool -q -if '(not \$CreateDate or (\$CreateDate eq "0000:00:00 00:00:00"))' -csv -common "$_"`;
+    # if(scalar(@result) > 1) {
+    # 	chomp @result;
+    # 	my @fields = split /,/, $result[1];
+    # 	print "$fields[0]\n";
+    # }
 }
 
 __DATA__
